316afcec044920fe1415348ff747e77cdb56d203
Hardik <hardik.pandey.716@gmail.com>
2025-05-07 00:28:08 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+# this is the core framework on which whole api is built
+from pydantic import BaseModel
+# this is the used for validation the req and res that we send and recieve the structure of it can be 
+# modified using pydantic
+from typing import List
+# 
+
+app = FastAPI()
+
+class Tea(BaseModel):
+    id: int
+    name: str
+    origin: str
+
+teas: List[Tea] = []
+# this is a list that contain the data structure Tea in it
+
+#Decorators: They give super power to the functions
+# In FastAPI, decorators are used to define API routes 
+# (endpoints) and associate them with specific HTTP methods like GET, POST, etc.
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Hardik Site"}
+
+@app.get("/teas")
+def get_teas():
+    return teas
+
+@app.post("/teas")
+def add_tea(tea: Tea): # we have pass the tea which we have to add through the function here
+    teas.append(tea)
+    return tea
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id: int, updated_tea: Tea):
+    for index,tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index] = update_tea
+            return update_tea
+        else:
+            return {"error": "Tea not found"}
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id: int):
+    for index,tea in enumerate(teas):
+        if tea.id == tea_id:
+            deleted = teas.pop(index) # here pop returns the value deleted which can be stored in a variable
+            return deleted
+        
+
+
